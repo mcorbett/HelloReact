2e3b4c0c9bf212e2fe15a638df1c8805f5dcadec
Matt Corbett <mcorbett@gmx.co.uk>
2016-05-02 23:01:09 +0100

    New files for Learnig React Project

---
+++ public/app.jsx
@@ -0,0 +1,39 @@
+var Greeter = React.createClass({
+    getDefaultProps: function () {
+        return {
+            name: 'React',
+            message: 'Do not enter your name and then press the button'
+        };
+    },
+    onButtonClick: function (e) {
+        e.preventDefault();
+        var name = this.refs.name.value
+
+        alert('I said don\'t press the fucking button '+name+', what is the matter with you.');
+    },
+    render: function () {
+        var name = this.props.name;
+        var message = this.props.message;
+
+        return (
+            <div>
+                <h1>Hello {name}</h1>
+                <p>{message}</p>
+
+            <form onSubmit={this.onButtonClick}>
+                <input type="text" ref="name"/>
+                <button>Set Name</button>
+            </form>
+        </div>
+
+        );
+    }
+});
+
+var firstName = 'Matt';
+var message = 'This is from the variable';
+
+ReactDOM.render(
+    <Greeter name={firstName} />,
+    document.getElementById('app')
+);
