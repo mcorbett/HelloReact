c48dd8042675cbd29019ec29aa587bcfe257ef10
mcorbett <mcorbett@mcorbett-ml.windows.ebi.ac.uk>
2016-05-03 16:58:40 +0100

    added functionality

--- public/app.jsx
+++ public/app.jsx
@@ -14,13 +14,16 @@ var Greeter = React.createClass({
 
     onButtonClick: function (e) {
         e.preventDefault();
-        var name = this.refs.name.value;
+        var nameRef = this.refs.name;
+        var name = nameRef.value;
+        nameRef.value = '';
 
-        this.setState ({
-          name: name
-        });
-        // alert('I said don\'t press the fucking button '+name+', what is the matter with you.');
-    },
+        if (typeof name === 'string' && name.length > 0) {
+          this.setState({
+            name: name
+          });
+        }
+      },
     render: function () {
         var name = this.state.name;
         var message = this.props.message;
